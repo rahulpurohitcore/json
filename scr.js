fae49c0776e80ee4d45f2d5a8888c066f013dd67
Rahul Purohit <rahul.purohit@celigo.com>
2024-03-31 04:08:44 +0000

    test

---
+++ scr.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const createLargeObject = () => {
+  let obj = {};
+  let str = 'a'.repeat(1024 * 5); // 5KB string
+
+  // Fill the object with the 5KB string 10 times to create a 50KB object
+  for (let i = 0; i < 10; i++) {
+    obj[`key${i}`] = str;
+  }
+
+  return obj;
+}
+
+// Create an array of 100 50KB objects to get a 5000KB (or approximately 5MB) JSON
+let largeArray = [];
+for (let i = 0; i < 100; i++) {
+  largeArray.push(createLargeObject());
+}
+
+let largeJson = JSON.stringify(largeArray);
+
+// Save the JSON to a file
+fs.writeFile('db.json', largeJson, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
